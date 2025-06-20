4486277593331485fd8e4a0b9e65162e46966040
Rakesh Adak <tech@kalingatechnology.com>
2025-06-20 17:28:04 +0530

    Menu updates

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+// Header Toggle Code
+const toggleBtn = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+toggleBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
 // Floating button code
-  const toggleButton = document.getElementById('toggleButton');
-  const menu = document.getElementById('menu');
+const toggleButton = document.getElementById("toggleButton");
+const menu = document.getElementById("menu");
 
-  let isOpen = false;
+let isOpen = false;
 
-  toggleButton.addEventListener('click', () => {
-    isOpen = !isOpen;
-    if (isOpen) {
-      menu.classList.remove('opacity-0', 'scale-0');
-      menu.classList.add('opacity-100', 'scale-100');
-    } else {
-      menu.classList.remove('opacity-100', 'scale-100');
-      menu.classList.add('opacity-0', 'scale-0');
-    }
-  });
+toggleButton.addEventListener("click", () => {
+  isOpen = !isOpen;
+  if (isOpen) {
+    menu.classList.remove("opacity-0", "scale-0");
+    menu.classList.add("opacity-100", "scale-100");
+  } else {
+    menu.classList.remove("opacity-100", "scale-100");
+    menu.classList.add("opacity-0", "scale-0");
+  }
+});
